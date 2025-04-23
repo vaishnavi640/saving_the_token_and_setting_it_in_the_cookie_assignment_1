fa920f0356ef7778e4635e40f2c5afb240e97cf0
vaishnavi640 <vaishnavikalluri@gmail.com>
2025-04-23 11:18:54 +0530

    changes

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-const encrypt = (payload) => {
+const jwt = require('jsonwebtoken');
+
+const encrypt = (payload) => { 
   // encrypt the payload and return token
-}
+  return jwt.sign(payload, 'root', { expiresIn: '1h' });
+};
 
 const decrypt = (token) => {
   // return decoded payload
-}
+  const decoded = jwt.verify(token, 'root');
+  return decoded;
+};
 
 module.exports = {
   encrypt,
   decrypt
-}
+};
